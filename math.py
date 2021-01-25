c14480866228b4a864bda7724151ef460bbe4e7e
Purva_Khadke <purva_khadke21@gmail.com>
2021-01-25 14:48:14 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# Perform addition
+def add(x,y):
+    return x+y
+# Perform subtraction
+def subtract(x,y):
+if x<y:
+return ERROR
+    return x-y
+# Perform multiplication
+def multiply(x,y):
+    pass
+    
+# Perform division
+def divide(x,y):
+    pass
